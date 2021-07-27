49c81b5564ac5d872bb6021dad1ce496f85174d0
robidev <gitlab@bohdziewicz.com.pl>
2021-07-27 21:11:21 +0200

    Every single step

--- src/main/java/pl/com/bohdziewicz/tddPractices/Franc.java
+++ src/main/java/pl/com/bohdziewicz/tddPractices/Franc.java
@@ -9,6 +9,6 @@ public class Franc extends Money {
 
     Money times(int multiplayer) {
 
-        return Money.franc(amount * multiplayer);
+        return new Franc(amount * multiplayer,"CHF");
     }
 }
