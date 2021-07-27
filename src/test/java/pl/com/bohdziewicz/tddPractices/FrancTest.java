bbaeba88e5fa6cc27bea7f17615e0ddcb56fb820
robidev <gitlab@bohdziewicz.com.pl>
2021-07-27 16:20:57 +0200

    Equality tests for Franc

--- src/test/java/pl/com/bohdziewicz/tddPractices/FrancTest.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/FrancTest.java
@@ -6,15 +6,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-
 public class FrancTest {
 
     @Test
-    public void testFrancMultiplaction(){
+    public void testFrancMultiplaction() {
 
         Franc five = new Franc(5);
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
     }
 
+    @Test
+    public void testEquality() {
+
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertTrue(new Franc(3).equals(new Franc(3)));
+        assertFalse(new Franc(3).equals(new Franc(5)));
+    }
 }
