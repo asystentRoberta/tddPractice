b93010b2d73ac5fff2ea8ecb2d32d94f0228c8a2
robidev <gitlab@bohdziewicz.com.pl>
2021-07-28 10:55:37 +0200

    Step 1 - simple addition test

--- src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
@@ -28,4 +28,10 @@ public class GeneralTests {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(3));
     }
+
+    @Test
+    public void testSimpleAddition(){
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
