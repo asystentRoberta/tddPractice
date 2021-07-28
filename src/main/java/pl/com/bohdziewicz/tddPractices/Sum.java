7c88e56cc160895d6eb664872af52b257510c2ce
robidev <gitlab@bohdziewicz.com.pl>
2021-07-28 15:06:35 +0200

    Step 3 Money.plus should return Sum

--- src/main/java/pl/com/bohdziewicz/tddPractices/Sum.java
+++ src/main/java/pl/com/bohdziewicz/tddPractices/Sum.java
@@ -1,7 +1,11 @@
 package pl.com.bohdziewicz.tddPractices;
 
-public class Sum {
+public class Sum implements Expression {
 
     Money augend;
     Money addend;
+
+    public Sum(Money augend, Money addend) {
+
+    }
 }
