50c0a2e6861cae725606891960662664ab67113f
robidev <gitlab@bohdziewicz.com.pl>
2021-07-29 23:52:41 +0200

    Step 1 - test we've waiting for - but... for now - we can't use it

--- src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
@@ -80,4 +80,17 @@ public class GeneralTests {
 
         assertEquals(1, new Bank().rate("USD", "USD"));
     }
+
+    //We've all been waiting for this :)
+
+    @Test
+    public void testMixedAddition() {
+
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
