bbab9ff9991ec64c66aa1a501996ea69ba7dd268
robidev <gitlab@bohdziewicz.com.pl>
2021-07-29 23:39:39 +0200

    Step 8a - previous change repaired testReduceMoneyDifferentCurrency test too

--- src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
@@ -69,10 +69,10 @@ public class GeneralTests {
     @Test
     public void testReduceMoneyDifferentCurrency() {
 
-//        Bank bank = new Bank();
-//        bank.addRate("CHF", "USD", 2);
-//        Money result = bank.reduce(Money.franc(2), "USD");
-//        assertEquals(Money.dollar(1), result);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
     }
 
     @Test
