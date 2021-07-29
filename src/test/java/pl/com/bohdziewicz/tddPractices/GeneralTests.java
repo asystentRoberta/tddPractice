79521f53856938bf645189df5cea4c16c566be5d
robidev <gitlab@bohdziewicz.com.pl>
2021-07-29 11:23:04 +0200

    Step 5 - we can't - delete test

--- src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
@@ -67,15 +67,11 @@ public class GeneralTests {
     }
 
     @Test
-    public void testReduceMoneyDifferentCurrency(){
+    public void testReduceMoneyDifferentCurrency() {
+
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
-    }
-
-    @Test
-    public void testArrayEquals(){
-        assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
     }
 }
