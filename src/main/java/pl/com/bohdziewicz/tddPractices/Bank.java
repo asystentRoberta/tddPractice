06db524152d0005d3269b82a3763aca63ff82389
robidev <gitlab@bohdziewicz.com.pl>
2021-07-29 11:19:52 +0200

    Step 3 - let rate goes to Bank

--- src/main/java/pl/com/bohdziewicz/tddPractices/Bank.java
+++ src/main/java/pl/com/bohdziewicz/tddPractices/Bank.java
@@ -6,4 +6,11 @@ public class Bank {
 
         return source.reduce(this, to);
     }
+
+    int rate(String from, String to) {
+
+        return (from.equals("CHF") && to.equals("USD"))
+                ? 2
+                : 1;
+    }
 }
