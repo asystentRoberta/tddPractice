b7429c41a83eb26b0ebce0e3f47aaf52ae24b6c5
robidev <gitlab@bohdziewicz.com.pl>
2021-07-29 23:37:48 +0200

    Step 8 - small change and big effect : test testIdentityRate is green - but wait see at next commit

--- src/main/java/pl/com/bohdziewicz/tddPractices/Bank.java
+++ src/main/java/pl/com/bohdziewicz/tddPractices/Bank.java
@@ -18,6 +18,8 @@ public class Bank {
 
     int rate(String from, String to) {
 
+        if (from.equals(to)) return 1;
+
         Integer rate = (Integer) rates.get(new Pair(from, to));
         return rate.intValue();
     }
