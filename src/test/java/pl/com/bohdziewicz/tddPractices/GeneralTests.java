f6b84a25bba44c64af1154d74f3ef7ff361f8ba1
robidev <gitlab@bohdziewicz.com.pl>
2021-07-30 13:46:02 +0200

    Step 3 - move method times to Sum class

--- src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
@@ -95,7 +95,8 @@ public class GeneralTests {
     }
 
     @Test
-    public void testSumPlusMoney(){
+    public void testSumPlusMoney() {
+
         Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
@@ -103,5 +104,17 @@ public class GeneralTests {
         Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
         Money result = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(15), result);
+    }
+
+    @Test
+    public void testSumTimes() {
+
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(20), "USD");
     }
 }
