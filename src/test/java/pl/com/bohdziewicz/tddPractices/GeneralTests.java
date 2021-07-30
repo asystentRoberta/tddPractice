8c042281796693e3554787d53f7ed1a3f5b60157
robidev <gitlab@bohdziewicz.com.pl>
2021-07-30 21:42:25 +0200

    Step 4b - fix mistake with comparison in assert

--- src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
+++ src/test/java/pl/com/bohdziewicz/tddPractices/GeneralTests.java
@@ -115,6 +115,6 @@ public class GeneralTests {
         bank.addRate("CHF", "USD", 2);
         Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
         Money result = bank.reduce(sum, "USD");
-        assertEquals(Money.dollar(20), "USD");
+        assertEquals(Money.dollar(20), result);
     }
 }
