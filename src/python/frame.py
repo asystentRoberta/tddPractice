15c330efee085b8f61c51db22c11b614357c932f
robidev <gitlab@bohdziewicz.com.pl>
2021-08-06 23:56:39 +0200

    Step 05 (GREEN) remove/rename setup to testTemplateMethod

--- src/python/frame.py
+++ src/python/frame.py
@@ -31,6 +31,6 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert ("setUp testMethod" == self.test.log)
