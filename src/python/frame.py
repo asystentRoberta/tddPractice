e9dc1d4605eea7b80da4586d27641d0160930e69
robidev <gitlab@bohdziewicz.com.pl>
2021-08-06 23:58:29 +0200

    Step 06 (RED) setup is redundant - only one instance of WasRun

--- src/python/frame.py
+++ src/python/frame.py
@@ -28,9 +28,6 @@ class WasRun(TestCase):
 
 class TestCaseTest(TestCase):
 
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
         self.test.run()
         assert ("setUp testMethod" == self.test.log)
